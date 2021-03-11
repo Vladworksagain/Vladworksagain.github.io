4c964fec8f8c3fa24ecce63eaf60a0c8c5dbee01
Vlad <vladworksagain@gmail.com>
2021-03-12 00:11:59 +0200

    .

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,72 @@ $(".select_location.second").select2({
     placeholder: "Выберите место получения",
     allowClear: true
 });
+
+const buttons = document.querySelectorAll('.modal_btn')
+
+const modals = document.querySelectorAll('.popup')
+
+const closeButtons = document.querySelectorAll('.popup_close-btn')
+
+
+closeButtons.forEach(buttonClose => {
+    buttonClose.addEventListener('click', closeModal, false)
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click', openModal, false)
+})
+
+function openModal(event) {
+    modals.forEach(item => {
+        if (event.target.dataset.button === item.dataset.modal) {
+            item.classList.add('open')
+        }
+    })
+}
+
+function closeModal(event) {
+    this.closest('.popup').classList.remove('open')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
