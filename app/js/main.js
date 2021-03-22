081957200aeef03e29444649152a3f487511c0d0
vladworksagain <vladworksagain@gmail.com>
2021-03-22 18:24:39 +0200

    start footer

--- app/js/main.js
+++ app/js/main.js
@@ -76,6 +76,22 @@ var swiperOffers = new Swiper('.offers_main-slider', {
     },
 });
 
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 5,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+
+
 
 
 
